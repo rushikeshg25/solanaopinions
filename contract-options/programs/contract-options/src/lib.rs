909a80da2214285f78ea10e86ba8a7e3162f44f9
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-12-30 00:00:22 +0530

    feat: init instruction and Errorcodes

--- contract-options/programs/contract-options/src/lib.rs
+++ contract-options/programs/contract-options/src/lib.rs
@@ -1,4 +1,8 @@
 use anchor_lang::prelude::*;
+mod states;
+use states::*;
+mod contexts;
+use contexts::*;
 
 declare_id!("J85ChLMWGQpRVu9VChUwFrfXnThNQRWho9ot8c4MFbFu");
 
@@ -7,9 +11,36 @@ pub mod contract_options {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let event_counter = &mut ctx.accounts.event_counter;
+        event_counter.count = 0;
+
+        let candidate_counter = &mut ctx.accounts.candidate_counter;
+        candidate_counter.count = 0;
+        Ok(())
+    }
+
+    pub fn create_event(
+        ctx: Context<CreateEvent>,
+        title: String,
+        start_date: u64,
+        end_date: u64,
+    ) -> Result<()> {
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The start date must be earlier than the end date")]
+    InvalidEvent,
+    #[msg("Cannot vote after the event has ended")]
+    EventClosed,
+    #[msg("Cannot vote before the event has started")]
+    EventNotStarted,
+    #[msg("Already voted")]
+    AlreadyVoted,
+    #[msg("Event does not exist")]
+    EventDoesNotExist,
+    #[msg("Candidate does not exist")]
+    CandidateDoesNotExist,
+}
